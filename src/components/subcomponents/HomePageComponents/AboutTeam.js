997e8c1688caf440b48c4626d367d039d0eeaee6
Asia <asia.dobrowolska95@gmail.com>
2020-11-23 18:18:42 +0100

    create subcomponent - AboutTeam

---
+++ src/components/subcomponents/HomePageComponents/AboutTeam.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const AboutTeam = () => {
+  return (
+    <section>
+      <h2>about us</h2>
+      <p>
+        Our international team do everything, that you can eat delicious pizza.
+        You can feel like in Italy. We prepare our pizza with the best
+        ingredients according to secret Italian recipe. Our team is properly
+        trained to prepare a real Italian pizza. You can order pizza online as
+        above instruction or by calling us. We have a choice of different types
+        of pizza - with meat, seafood or vege. You will be happy!
+      </p>
+    </section>
+  );
+};
+
+export default AboutTeam;
