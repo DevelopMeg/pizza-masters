9834b2c2f6f747b72635a67015e722d77491a6ae
Asia <asia.dobrowolska95@gmail.com>
2020-11-30 15:49:24 +0100

    create route component - OrderPage

---
+++ src/components/route-components/OrderPage.js
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from "react";
+import SearchCategories from "../subcomponents/OrderPageComponents/SearchCategories";
+
+const OrderPage = ({ menuItems, menuExtraIngredients, menuSauces }) => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+  // CATEGORIES MENU
+
+  const initialMenuCategories = [
+    { name: "all", choosed: false },
+    { name: "popular", choosed: false },
+    { name: "meat pizza", choosed: false },
+    { name: "seafood pizza", choosed: false },
+    { name: "vege pizza", choosed: false },
+  ];
+
+  const [menuCategories, setMenuCategories] = useState(initialMenuCategories);
+
+  const [menuItemsCategories, setMenuItemsCategories] = useState([]);
+
+  const handleChoosedMenuCategories = (chooseCategory) => {
+    setMenuCategories(
+      menuCategories.map((category) => {
+        return category.name === chooseCategory
+          ? { ...category, choosed: true }
+          : { ...category, choosed: false };
+      })
+    );
+  };
+
+  const handleMenuItemsCategories = (categoryName) => {
+    let categoryForItem = [];
+
+    menuItems.forEach((item) => {
+      const result = item.categories.includes(categoryName);
+      result && categoryForItem.push(item);
+    });
+
+    if (categoryName !== "all") {
+      setMenuItemsCategories(categoryForItem);
+    } else {
+      setMenuItemsCategories([]);
+    }
+
+    handleChoosedMenuCategories(categoryName);
+  };
+
+  return (
+    <section>
+      <h2 extraMargin>choose your pizza</h2>
+
+      <SearchCategories
+        menuCategories={menuCategories}
+        handleMenuItemsCategories={handleMenuItemsCategories}
+      />
+    </section>
+  );
+};
+
+export default OrderPage;
